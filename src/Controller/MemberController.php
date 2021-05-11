1aa43f413d58bdd49a899fb562712c3bc59dbe56
Quentin <quentin.burty@gmail.com>
2021-05-11 16:33:39 +0200

    register + login

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -9,49 +9,59 @@ use App\Model\MemberManager;
 class MemberController extends AbstractController
 {
     public function login()
-
     {
+        if (isset($_SESSION["user"])) {
+            header('Location: /activity/index');
+        }
+        session_start();
+        $error = '';
+
         if (!empty($_POST)) {
             $pseudo = $_POST['pseudo'];
             $password = $_POST['password'];
-            $memberManager = new MemberManager();
-            $nameArray = $memberManager->selectAll($pseudo);
+            $memberArray = (new MemberManager())->selectOneByName($pseudo);
+            
 
             if (
-                !empty($nameArray) &&
-                $pseudo === $nameArray['pseudo'] &&
-                password_verify($password, $nameArray['mot_de_passe'])
+                !empty($_POST) &&
+                password_verify($password, $memberArray['password'])
             ) {
-                $nameArray['est_connecte'] = true;
-                $_SESSION['member'] = $nameArray;
-                header('Location: /Activity/index');
+                $_SESSION['user'] = $memberArray;
+                header('Location: /home/index');
             } else {
                 $error = 'Identifiants incorrects';
             }
         }
 
-        return $this->twig->render('home/index.html.twig', [
+        return $this->twig->render('member/login.html.twig', [
             'error' => $error,
         ]);
 
-        return $this->twig->render('Member/signup.html.twig');
+        return $this->twig->render('Member/login.html.twig');
     }
 
     public function signUp()
     {
-        if ($_POST['password'] === $_POST['repeatpassword']) {
-            $MemberManager = new MemberManager();
-            $passwordHashed = password_hash($_POST['password'], PASSWORD_BCRYPT);
-            $MemberManager->insert([
-                'pseudo' => trim($_POST['pseudo']),
-                'password' => trim($passwordHashed),
-            ]);
-            $nameArray = $MemberManager->selectAll($_POST['pseudo']);
-            $nameArray['est_connecte'] = true;
-            $_SESSION['user'] = $nameArray;
-            header('Location: /Activity/index');
-        } else {
-            header('Location: /Home/index');
+        if (!empty($_POST))
+        {
+            if ($_POST['password'] === $_POST['repeatpassword']) {
+                $MemberManager = new MemberManager();
+                $passwordHashed = password_hash($_POST['password'], PASSWORD_BCRYPT);
+                $MemberManager->insert([
+                    'name' => trim($_POST['pseudo']),
+                    'password' => trim($passwordHashed),
+                    'bio' => trim($_POST['bio']),
+                    'favorite_activity' => trim($_POST['fav-activity']),
+                    
+                ]);
+                $nameArray = $MemberManager->selectOneByName($_POST['pseudo']);
+                $nameArray['is_logged'] = true;
+                $_SESSION['user'] = $nameArray;
+                header('Location: /home/index');
+            } else {
+                header('Location: /Home/index');
+            }
         }
+        return $this->twig->render('Member/signUp.html.twig');
     }
 }
