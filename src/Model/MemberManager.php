6d7f96ce0e70942a9f14010a583a816d511e89f2
Quentin <quentin.burty@gmail.com>
2021-05-11 13:36:41 +0200

    members managers forwarded

--- src/Model/MemberManager.php
+++ src/Model/MemberManager.php
@@ -2,31 +2,38 @@
 
 namespace App\Model;
 
-class ItemManager extends AbstractManager
+class MemberManager extends AbstractManager
 {
-    public const TABLE = 'item';
+    public const TABLE = 'member';
 
     /**
-     * Insert new item in database
+     * Insert new member in database
      */
-    public function insert(array $item): int
+    public function insert(array $member)
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`) VALUES (:title)");
-        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `password`, `image`, `is_logged`, `favorite_activity`, `bio`) 
+                                        VALUES (:name, :password, :image, :is_logged, :favorite_activity, :bio)");
+        $statement->bindValue('name', $member['name'], \PDO::PARAM_STR);
+        $statement->bindValue('password', $member['password'], \PDO::PARAM_STR);
+        $statement->bindValue('image', $member['image'], \PDO::PARAM_STR);
+        $statement->bindValue('is_logged', $member['is_logged'], \PDO::PARAM_BOOL);
+        $statement->bindValue('favorite_activity', $member['favorite_activity'], \PDO::PARAM_INT);
+        $statement->bindValue('bio', $member['bio'], \PDO::PARAM_STR);
 
-        $statement->execute();
-        return (int)$this->pdo->lastInsertId();
+        return $statement->execute();
+        // return (int)$this->pdo->lastInsertId();
     }
 
     /**
-     * Update item in database
+     * Select one member with his name
      */
-    public function update(array $item): bool
+    public function selectOneByName($name)
     {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
-        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
 
-        return $statement->execute();
+        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE name=:name");
+        $statement->bindValue('name', $name, \PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetch();
     }
 }
